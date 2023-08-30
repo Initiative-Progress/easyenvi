ece1467ca7ae87ad4685380789e9e7a79a5f77e3
Antoine Pinto <antoine.pinto1@outlook.fr>
2023-08-30 20:55:29 +0200

    Add pdf, docx, pdf and sql extensions

--- easyenvi/file_manager.py
+++ easyenvi/file_manager.py
@@ -2,21 +2,28 @@ class FileManager:
     """
     Manages loading and saving files using various formats.
     
-    Arguments:
-        extra_loader_config (dict): Extra configuration for file loaders.
-        extra_saver_config (dict): Extra configuration for file savers.
+    Parameters
+    ----------
+    extra_loader_config : dict
+        Extra configuration for file loaders.
+    extra_saver_config : dict
+        Extra configuration for file savers.
     """
 
     def __init__(self, extra_loader_config=None, extra_saver_config=None):
 
         self.loader_config = {
             'csv':      (csv_loader,        'rb'),
+            'docx':     (docx_loader,       'rb'),
             'jpg':      (jpg_loader,        'rb'),
             'json':     (json_loader,       'rt'),
             'md':       (md_loader,         'rt'),
             'parquet':  (parquet_loader,    'rb'),
+            'pdf':      (pdf_loader,        'rb'),
             'pickle':   (pickle_loader,     'rb'),
             'png':      (png_loader,        'rb'),
+            'pptx':     (pptx_loader,       'rb'),
+            'sql':      (sql_loader,        'rt'),
             'toml':     (toml_loader,       'rt'),
             'txt':      (txt_loader,        'rt'),
             'xlsx':     (xlsx_loader,       'rb'),
@@ -27,14 +34,18 @@ class FileManager:
         
         self.saver_config = {
             'csv':      (csv_saver,         'wb'),
+            'docx':     (docx_saver,        'wb'),
             'jpg':      (jpg_saver,         'wb'),
             'json':     (json_saver,        'wt'),
             'md':       (md_saver,          'wt'),
             'parquet':  (parquet_saver,     'wb'),
+            'pdf':      (pdf_saver,         'wb'),
             'pickle':   (pickle_saver,      'wb'),
             'png':      (png_saver,         'wb'),
+            'pptx':     (pptx_saver,        'wb'),
+            'sql':      (sql_saver,         'wt'),
             'toml':     (toml_saver,        'wt'),
-            'txt':      (txt_saver,        'wt'),
+            'txt':      (txt_saver,         'wt'),
             'xlsx':     (xlsx_saver,        'wb'),
             'xml':      (xml_saver,         'wb'),
             'yaml':     (yaml_saver,        'wt'),
@@ -54,6 +65,14 @@ def csv_loader(path, **kwargs):
 
 def csv_saver(obj, path, **kwargs):
     obj.to_csv(path, **kwargs)
+
+# DOCX
+def docx_loader(path):
+    from docx import Document
+    return Document(path)
+
+def docx_saver(obj, path):
+    obj.save(path)
 
 # JPG
 def jpg_loader(path, **kwargs):
@@ -87,6 +106,21 @@ def parquet_loader(path, **kwargs):
 def parquet_saver(obj, path, **kwargs):
     obj.to_parquet(path, **kwargs)
 
+# PDF
+def pdf_loader(path):
+    from PyPDF2 import PdfReader
+    # return PdfReader(path)
+    import copy
+    return copy.deepcopy(PdfReader(path))
+
+def pdf_saver(obj, path):
+    from PyPDF2 import PdfWriter
+    output = PdfWriter()
+    for page in obj.pages:
+        output.add_page(page)
+    
+    output.write(path)
+
 # PICKLE
 def pickle_loader(path, **kwargs):
     import pickle
@@ -96,6 +130,14 @@ def pickle_saver(obj, path, **kwargs):
     import pickle
     pickle.dump(obj, path, protocol=pickle.HIGHEST_PROTOCOL, **kwargs)
 
+# PPTX
+def pptx_loader(path):
+    from pptx import Presentation
+    return Presentation(path)
+
+def pptx_saver(obj, path):
+    obj.save(path)
+
 # PNG
 def png_loader(path, **kwargs):
     from PIL import Image
@@ -103,6 +145,13 @@ def png_loader(path, **kwargs):
 
 def png_saver(obj, path, **kwargs):
     obj.save(path, **kwargs)
+
+# SQL
+def sql_loader(path, **kwargs):
+    return path.read(**kwargs)
+
+def sql_saver(obj, path, **kwargs):
+    path.write(obj, **kwargs)
 
 # TOML
 def toml_loader(path, **kwargs):
